a6a32179c8d79230fbb8fc37550b4bdbb1774b8e
Babu <homeworks@testleaf.com>
2020-10-21 16:01:31 +0530

    Baseline code for Eye Management

---
+++ src/main/java/com/app/eyes/controller/PatientRestController.java
@@ -0,0 +1,55 @@
+package com.app.eyes.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.eyes.model.Response;
+import com.app.eyes.pojo.PatientDetailsPOJO;
+import com.app.eyes.service.PatientService;
+
+@RestController
+@RequestMapping("/dashboard")
+public class PatientRestController {
+	
+	@Autowired
+	PatientService patientService;
+	
+	private static final Logger logger = Logger.getLogger(PatientRestController.class);
+	
+	@RequestMapping(value= "/patientSearch/patientDelete",method=RequestMethod.POST)
+	public Response deletePatient(@RequestBody PatientDetailsPOJO p ) {
+		logger.info("Patient Delete Post");
+		logger.info("Patient Id : " + p.getPatientId());
+		try {
+			patientService.delete(p);
+			return new Response("Done",p);
+		} catch (Exception e) {
+			logger.info("Could Not Delete : ", e);
+			return new Response("Error",p);
+		}
+	}
+	
+	@RequestMapping(value= "/patientSearch/patientEdit",method=RequestMethod.POST)
+	public Response editPatient(@RequestBody PatientDetailsPOJO p ) {
+		logger.info("Patient Edit Post");
+		logger.info("Patient Id : " + p.getPatientId());
+		try {
+			if (1 == patientService.editPatientDetails(p.getPatientId(),p.getTitle(), p.getFirstName(), p.getMiddleName(), p.getLastName(),
+					p.getGender(), p.getEmail(), p.getMobile(), p.getPhoneNumber())) {
+				logger.info("Edited Successfully");
+				logger.info(p);
+				return new Response("Done",p);
+			}
+			else
+				return new Response("Error",p);
+		} catch (Exception e) {
+			logger.info("Could Not Edit : ", e);
+			return new Response("Error",p);
+		}
+	}
+	
+}
